743dc4cf36b9449ceaec72d6189504a52c4c37b7
GeorgeU2030 <georgeu2021@gmail.com>
2025-02-07 12:18:02 -0500

    setup webapi

---
+++ Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+// Message for the root path
+app.MapGet("/", () => "Whisp backend running...!");
+
+app.Run();
+
+
